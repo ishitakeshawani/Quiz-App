b26a9b0d47942edc5152a624926fdb054056d6ae
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-05-01 23:26:05 +0530

    added routes to different file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
+import Routes from "./Routes";
 import "./App.css";
-import { HomePage } from "./pages";
-import { Routes, Route } from "react-router-dom";
 import { NavBar } from "./components";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <Routes />
     </div>
   );
 }
