c17b53c8f29f1f907288302ba3b7d8192d30ec5b
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-05-01 17:15:43 +0530

    added quiz context

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
+import { QuizProvider } from "./contexts";
 
 // Call make Server
 makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QuizProvider>
+        <App />
+      </QuizProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
