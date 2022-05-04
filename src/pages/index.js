250c2a823641499367470de9d7af5fc9aa5ace92
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-05-05 02:53:10 +0530

    added export for quizpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { HomePage } from "./homepage/HomePage";
 export { CategoryPage } from "./categories-page/CategoryPage";
 export { RulesPage } from "./rules-page/RulesPage";
+export { QuizPage } from "./quiz-page/QuizPage";
