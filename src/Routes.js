89ebbf588e7b6f1cc1ec96ea39062443e7abf6af
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-05-05 02:49:49 +0530

    added route for quiz page

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { CategoryPage, HomePage, RulesPage } from "./pages";
+import { CategoryPage, HomePage, QuizPage, RulesPage } from "./pages";
 
 export default function routes() {
   return (
@@ -8,6 +8,7 @@ export default function routes() {
       <Route path="/" element={<HomePage />} />
       <Route path="/category/:categoryName" element={<CategoryPage />} />
       <Route path="/rules/:quizName" element={<RulesPage />} />
+      <Route path="/quiz/:quizName" element={<QuizPage />} />
     </Routes>
   );
 }
