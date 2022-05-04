9187e6a7f0114c0a077c8686c5d97d98bee9943e
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-05-05 02:54:11 +0530

    added questions and options in quiz page

--- src/pages/rules-page/RulesPage.js
+++ src/pages/rules-page/RulesPage.js
@@ -7,16 +7,16 @@ export function RulesPage() {
   const { quizName } = useParams();
   return (
     <div>
-      <div class="quiz-rule-page flex-row">
-        <div class="quiz-rules flex-row">
-          <div class="quiz-rule-title">{quizName}</div>
-          <div class="quiz-rules-list">
+      <div className="quiz-rule-page flex-row">
+        <div className="quiz-rules flex-row">
+          <div className="quiz-rule-title">{quizName}</div>
+          <div className="quiz-rules-list">
             <p>1. Each right answer scores 10 Points</p>
             <p>2. Each multiple choice question has only one correct answer</p>
             <p>3. To win the quiz you need to score more than 70%</p>
           </div>
-          <Link to="/">
-            <button class="btn start-quiz-btn">Start Quiz</button>
+          <Link to={`/quiz/${quizName}`}>
+            <button className="btn start-quiz-btn">Start Quiz</button>
           </Link>
         </div>
       </div>
--- src/reducers/quizReducer.js
+++ src/reducers/quizReducer.js
@@ -6,6 +6,34 @@ export const quizReducer = (quizState, { type, payload }) => {
     case "SET_QUIZZES":
       return { ...quizState, quizList: payload };
 
+    case "SET_CURRENT_QUESTION_INDEX":
+      return { ...quizState, currentQuestionIndex: payload };
+
+    case "SET_SELECTED_OPTION":
+      return {
+        ...quizState,
+        quizList: quizState.quizList.map((quiz) =>
+          quiz._id === payload.quizId
+            ? {
+                ...quiz,
+                score: quiz.score + payload.score,
+                questionsList: quiz.questionsList.map((question) =>
+                  question._id === payload.questionId
+                    ? {
+                        ...question,
+                        options: question.options.map((option) =>
+                          option._id == payload.optionId
+                            ? { ...option, isSelected: payload.isSelected }
+                            : option
+                        ),
+                      }
+                    : question
+                ),
+              }
+            : quiz
+        ),
+      };
+
     default:
       return quizState;
   }
